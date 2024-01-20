05e28691b04102abbe7e182d18320541558caa80
waterrminn <112816841+waterrminn@users.noreply.github.com>
2024-01-21 01:10:48 +0900

    Update optimizers.py

--- src/training/optimizers.py
+++ src/training/optimizers.py
@@ -2,18 +2,18 @@ from torch.optim import AdamW
 
 
 
-def get_optimizer_params(model, encoder_lr, decoder_lr, weight_decay=0.0):
+def get_optimizer_params(model, #, #, weight_decay=0.0):
     """
     Optimizer parameters by encoder and decoder
     """
     param_optimizer = list(#)
     no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
     optimizer_parameters = [
-        {'params': [p for n, p in model.llm_model.named_parameters() if not any(nd in n for nd in no_decay)],
+        {'params': [p for n, p in # if not any(nd in n for nd in no_decay)],
         'lr': #, 'weight_decay': #},
-        {'params': [p for n, p in model.llm_model.named_parameters() if any(nd in n for nd in no_decay)],
+        {'params': [p for n, p in # if any(nd in n for nd in no_decay)],
         'lr': #, 'weight_decay': #},
-        {'params': [p for n, p in model.named_parameters() if "model" not in n],
+        {'params': [p for n, p in # if "model" not in n],
         'lr': #, 'weight_decay': #}
     ]
     return optimizer_parameters
@@ -21,8 +21,8 @@ def get_optimizer_params(model, encoder_lr, decoder_lr, weight_decay=0.0):
 
 def get_optimizer(cfg, model):
     """ Select the optimizer """
-    if cfg.name == 'AdamW':
-        opt = AdamW(filter(lambda p: p.requires_grad, model.parameters()),
+    if cfg.name == #:
+        opt = #(filter(lambda p: p.requires_grad, model.parameters()),
                     lr=#,
                     )
     else:
